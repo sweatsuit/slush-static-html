1b253925ac0161c65a9812918c927e7d8e876d1b
Jim Parish <jparish@pyramidcommunications.com>
2015-10-14 23:02:04 -0700

    add sass & sass watch to gulp file

--- templates/Gulpfile.js
+++ templates/Gulpfile.js
@@ -1,5 +1,16 @@
 var gulp = require('gulp'),
-  connect = require('gulp-connect');
+  connect = require('gulp-connect'),
+  sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+  gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
 
 gulp.task('connect', function() {
   connect.server({
@@ -15,7 +26,7 @@ gulp.task('reload', function () {
 });
 
 gulp.task('watch', function () {
-  gulp.watch(['./**/*.html','js/main.js', 'js/plugins.js', 'css/main.css'], ['reload']);
+  gulp.watch(['./**/*.html','js/main.js', 'js/plugins.js', 'css/styles.css'], ['reload']);
 });
 
-gulp.task('default', ['connect', 'watch']);
+gulp.task('default', ['connect', 'watch', 'sass:watch']);
